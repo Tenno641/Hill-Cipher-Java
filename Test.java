33204eb830c96d6b13c12750ad3d52f9a00d2c4a
Tenno641 <ahmedfotoh2003@gmail.com>
2023-08-20 23:52:47 +0300

    Day4

--- Test.java
+++ Test.java
@@ -1,11 +1,35 @@
 public class Test {
 
+    public static int[] sort(int[] arr) {
+
+        int size = arr.length;
+
+        for (int i = 0; i < size - 1; i++) {
+
+            for (int j = 0; j < size - 1; j++) {
+
+                if (arr[j] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j] = temp;
+                }
+
+            }
+
+        }
+
+
+    }
+
     public static void main(String[] args) {
 
-        Main main = new Main("Ahmed");
-        main.encrypt("Ahmed");
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        int[] sortedArr = sort(arr);
 
+        for (int n : sortedArr) {
+            System.out.print(n);
+        }
 
     }
 
